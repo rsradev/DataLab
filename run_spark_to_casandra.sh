5788da4701388debc13bbb1f29ee1cd7e3538114
raddystr <raddy.radev@gmail.com>
2023-03-25 23:55:04 +0200

    structure update

--- run_spark_to_casandra.sh
+++
@@ -1,1 +0,0 @@
-spark-submit --packages com.datastax.spark:spark-cassandra-connector_2.12:3.1.0 CassandraSpark.py
---
+++ templates/kafka_spark_template/StreamHandler/run_job.sh
@@ -0,0 +1,1 @@
+sbt package && /home/raddy/Downloads/opt/spark-2.4.5-bin-hadoop2.7/bin/spark-submit --class StreamHandler --master local[*] --packages "org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,com.datastax.spark:spark-cassandra-connector_2.11:2.4.3,com.datastax.cassandra:cassandra-driver-core:4.0.0" target/scala-2.11/stream-handler_2.11-1.0.jar 
